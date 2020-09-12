763ae37331841dd7d9d9de735b5eec34eedbe1bc
Satoqz <satoqzzz@gmail.com>
2020-09-12 02:30:00 +0200

    everything in MessageReq should be optional

--- src/typings/Request/Message.ts
+++ src/typings/Request/Message.ts
@@ -2,11 +2,11 @@ import { Embed } from "../Embed.ts";
 
 export interface MessageReq
 {
-	content: string
-	nonce: number | string
-	tts: boolean
-	file: unknown
-	embed: Embed
-	payload_json: string
-	allowed_mentions: unknown
+	content?: string
+	nonce?: number | string
+	tts?: boolean
+	file?: unknown
+	embed?: Embed
+	payload_json?: string
+	allowed_mentions?: unknown
 }
